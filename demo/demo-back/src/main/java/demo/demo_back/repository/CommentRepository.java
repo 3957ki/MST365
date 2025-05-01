d3812e970ac8198903c8b25fc7e01d8e229a7497
장은정 <eunjung5406@naver.com>
2025-05-01 17:17:46 +0900

    :recycle: [refactor] 전체댓글조회 삭제된 댓글 제외

--- demo/demo-back/src/main/java/demo/demo_back/repository/CommentRepository.java
+++ demo/demo-back/src/main/java/demo/demo_back/repository/CommentRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
     List<Comment> findByBoardId(Long boardId);
 
     List<Comment> findByBoard_IdAndIsDeletedFalseOrderByCreatedAtAsc(Long boardId);
 
     List<Comment> findByUserId(Long userId);
 
+    List<Comment> findByUserIdAndIsDeletedFalse(Long userId);
 }
