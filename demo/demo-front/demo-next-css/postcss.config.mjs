8f038d0a1af1dc1fa08fbf77a05bf3b5bf34f8a7
장은정 <eunjung5406@naver.com>
2025-05-11 18:36:26 +0900

    :fire: [remove] next-css 파일 삭제

--- demo/demo-front/demo-next-css/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}, // autoprefixer 플러그인 추가
-  },
-};
-
-export default config;
