391e6a1a68124abe15b25f48e9bef32037c4a86d
장은정 <eunjung5406@naver.com>
2025-05-12 14:04:22 +0900

    :recycle: [refactor] WebController 수정

--- demo/demo-back/src/main/java/demo/demo_back/controller/WebController.java
+++ demo/demo-back/src/main/java/demo/demo_back/controller/WebController.java
@@ -1,2 +1,21 @@
-package demo.demo_back.controller;public class WebController {
+package demo.demo_back.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping({
+            "/",              // 메인 페이지
+            "/login",         // 로그인
+            "/signup",        // 회원가입
+            "/board",         // 게시판 목록
+            "/board/**",      // 게시판 상세
+            "/mypage",        // 마이페이지
+            "/admin/**"       // 관리자 영역
+    })
+    public String index() {
+        return "forward:/index.html";
+    }
 }
